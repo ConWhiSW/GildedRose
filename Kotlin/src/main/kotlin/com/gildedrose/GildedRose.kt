0bba9cd99510e92e8b1418c16f286745dbb53edd
ConWhiSW <connor.whitworth@softwire.com>
2024-09-18 17:24:17 +0100

    Create update functions

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,6 +2,27 @@ package com.gildedrose
 
 class GildedRose(var items: List<Item>) {
 
+    fun updateBrie(): Int {
+        return 1
+    }
+
+    fun updateSulfuras(): Int {
+        return 0
+    }
+
+    fun updateBackstagePass(item: Item): Int {
+        if (item.sellIn > 10) {
+            return 1
+        }
+        if (item.sellIn > 5) {
+            return 2
+        }
+        if (item.sellIn > 0) {
+            return 3
+        }
+        return -1*item.quality
+    }
+
     fun updateQuality() {
         for (i in items.indices) {
             if (items[i].name != "Aged Brie" && !items[i].name.startsWith("Backstage passes")) {
