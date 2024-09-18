7882a5c055b6a52f6aab0bca15307ab252866263
ConWhiSW <connor.whitworth@softwire.com>
2024-09-18 17:27:55 +0100

    Remove counter

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -24,54 +24,54 @@ class GildedRose(var items: List<Item>) {
     }
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && !items[i].name.startsWith("Backstage passes")) {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality -= 1
-                        if (items[i].name.startsWith("Conjured")) {
-                            items[i].quality -= 1
+        for (item in items) {
+            if (item.name.lowercase().indexOf("sulfuras") == -1) {
+                item.sellIn -= 1
+            }
+
+            if (item.name != "Aged Brie" && !item.name.startsWith("Backstage passes")) {
+                if (item.quality > 0) {
+                    if (item.name != "Sulfuras, Hand of Ragnaros") {
+                        item.quality -= 1
+                        if (item.name.startsWith("Conjured")) {
+                            item.quality -= 1
                         }
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality += 1
+                if (item.quality < 50) {
+                    item.quality += 1
 
-                    if (items[i].name.startsWith("Backstage passes")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality += 1
+                    if (item.name.startsWith("Backstage passes")) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality += 1
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality += 1
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality += 1
                             }
                         }
                     }
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn -= 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (!items[i].name.startsWith("Backstage passes")) {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality -= 1
+            if (item.sellIn < 0) {
+                if (item.name != "Aged Brie") {
+                    if (!item.name.startsWith("Backstage passes")) {
+                        if (item.quality > 0) {
+                            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                                item.quality -= 1
                             }
                         }
                     } else {
-                        items[i].quality -= items[i].quality
+                        item.quality -= item.quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality += 1
+                    if (item.quality < 50) {
+                        item.quality += 1
                     }
                 }
             }
